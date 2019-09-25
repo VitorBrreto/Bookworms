8134a2ecb8e3888e09a8a148ec9a9308c3fb6f63
Paulo Vitor Barreto <pvitoreu@gmail.com>
2019-09-24 23:21:33 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text></Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
